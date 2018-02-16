beb8a40ca84945d83b3413bf0dad18a9d15b2a69
brianmknapp <35432266+brianmknapp@users.noreply.github.com>
2018-02-16 12:19:25 -0500

    Add missing file argument handle

--- src/MainKnapp1.java
+++ src/MainKnapp1.java
@@ -1,6 +1,9 @@
 public class MainKnapp1 {
 
     public static void main(String[] args) {
-
+        if (args.length != 1) {
+            System.out.println("Usage: <fullFilePath>");
+            System.exit(1);
+        }
     }
 }
