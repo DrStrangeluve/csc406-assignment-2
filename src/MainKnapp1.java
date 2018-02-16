09ba23b75d25c2fe91c18d2b8ffef352f360cb3c
brianmknapp <35432266+brianmknapp@users.noreply.github.com>
2018-02-16 12:24:03 -0500

    Add instantiation of Sudoku2Sat
    The SudokuToSatReducerKnapp1 class will take in a File object. In the Main class the file path string argument is converted to a File in the instantiation.
    
    feature/part1

--- src/MainKnapp1.java
+++ src/MainKnapp1.java
@@ -1,9 +1,14 @@
+import java.io.File;
+
 public class MainKnapp1 {
 
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: <fullFilePath>");
             System.exit(1);
+        }
+        else {
+            SudokuToSatReducerKnapp1 sudoku_sat_instance = new SudokuToSatReducerKnapp1(new File(args[0]));
         }
     }
 }
